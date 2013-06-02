7763e4e5f95a89ae9e1e8d08bc2eecde8c3c3962
andrenoer <andrenoer@gmail.com>
2013-06-02 16:34:34 -0300

    Adição da entidade Sexo.

--- src/main/java/br/com/semeru/util/HibernateUtil.java
+++ src/main/java/br/com/semeru/util/HibernateUtil.java
@@ -6,24 +6,34 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
-
-    private static final SessionFactory sessionFactory ;
+    
+    private static final SessionFactory sessionFactory;
     public static final String HIBERNATE_SESSION = "hibernate_session";
-
-    static {
+    
+    static{
+        
         try {
-            System.out.println("Tentando configurar uma Session Factory...");
+            
+            System.out.println("Tentando configurar a SF");
+            
             Configuration configuration = new Configuration().configure();
-            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
+                applySettings(configuration.getProperties()).buildServiceRegistry();
+            
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            System.out.println("Session Factory criada corretamente.");
+            
+            System.out.println("Session factory criada corretamente");
         } catch (Exception ex) {
-            System.out.println("Ocorreu um erro ao iniciar a Session Factory. " + ex);
+            System.out.println("Ocorreu um  erro ao iniciar a SF. " + ex);
             throw new ExceptionInInitializerError(ex);
         }
+        
+        
     }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+    
 }
